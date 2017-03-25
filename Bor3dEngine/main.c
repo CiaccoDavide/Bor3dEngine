5ad11012889e7efab9a12d5c9c5e823ea2e9f207
CiaccoDavide <ciaccodavide@gmail.com>
2017-03-25 10:18:03 +0100

    first triangle

--- Bor3dEngine/main.c
+++ Bor3dEngine/main.c
@@ -11,12 +11,60 @@ int WindowHandle = 0; // used to store the handle to the window created by FreeG
 
 unsigned FrameCount = 0;
 
+//
+typedef struct
+{
+	float XYZW[4];
+	float RGBA[4];
+} Vertex;
+
+GLuint VertexShaderId;
+GLuint FragmentShaderId;
+GLuint ProgramId;
+GLuint VaoId;
+GLuint VboId;
+
+// vertex shader
+const GLchar* VertexShader =
+{
+	"#version 400 /* using GLSL version 4.00 */\n"\
+
+	"layout(location=0) in vec4 in_Position;\n"\
+	"layout(location=1) in vec4 in_Color;\n"\
+	"out vec4 ex_Color;\n"\
+
+	"void main(void)\n"\
+	"{\n"\
+	"  gl_Position = in_Position;\n"\
+	"  ex_Color = in_Color;\n"\
+	"}\n"
+};
+// fragment shader (affects the final color of each pixel)
+const GLchar* FragmentShader =
+{
+	"#version 400 /* using GLSL version 4.00 */\n"\
+
+	"in vec4 ex_Color;\n"\
+	"out vec4 out_Color;\n"\
+
+	"void main(void)\n"\
+	"{\n"\
+	"  out_Color = ex_Color;\n"\
+	"}\n"
+};
+
 void Initialize(int, char*[]);
 void InitWindow(int, char*[]);
 void ResizeFunction(int, int);
 void RenderFunction(void);
 void TimerFunction(int);
 void IdleFunction(void);
+//
+void Cleanup(void);
+void CreateVBO(void);
+void DestroyVBO(void);
+void CreateShaders(void);
+void DestroyShaders(void);
 
 int main(int argc, char* argv[])
 {
@@ -32,6 +80,8 @@ void Initialize(int argc, char* argv[])
 	GLenum GlewInitResult;
 
 	InitWindow(argc, argv); // glutInit() setup
+
+	glewExperimental = GL_TRUE;
 
 	GlewInitResult = glewInit();
 
@@ -49,6 +99,9 @@ void Initialize(int argc, char* argv[])
 		"INFO: OpenGL Version: %s\n",
 		glGetString(GL_VERSION)
 		);
+
+	CreateShaders();
+	CreateVBO();
 
 	glClearColor(0.1f, 0.1f, 0.1f, 0.0f); // background color
 }
@@ -88,6 +141,8 @@ void InitWindow(int argc, char* argv[])
 	glutDisplayFunc(RenderFunction); // links the function that will manage the scene rendering
 	glutIdleFunc(IdleFunction);
 	glutTimerFunc(0, TimerFunction, 0); // remember that v-sync depends on the gpu driver settings!
+
+	glutCloseFunc(Cleanup);
 }
 
 void ResizeFunction(int Width, int Height)
@@ -104,6 +159,7 @@ void RenderFunction(void)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // reset some buffers: back-buffer (to glClearColor) and z-buffer
 
 	// here we can draw on the back-buffer
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
 
 	glutSwapBuffers(); // swaps the back-buffer with the front-buffer, rendering the drawn scene to our window!
 }
@@ -134,4 +190,136 @@ void TimerFunction(int Value)
 
 	FrameCount = 0;
 	glutTimerFunc(250, TimerFunction, 1);
+}
+
+
+void Cleanup(void)
+{
+	DestroyShaders();
+	DestroyVBO();
+}
+
+void CreateVBO(void)
+{	
+	// {{vertex position},{vertex color}} 
+	Vertex Vertices[] =
+	{
+		{ { -0.6f, -0.6f, 0.0f, 1.0f },{ 1.0f, 0.2f, 0.2f, 1.0f } },
+		{ { 0.0f,  0.6f, 0.0f, 1.0f },{ 0.2f, 1.0f, 0.2f, 1.0f } },
+		{ { 0.6f, -0.6f, 0.0f, 1.0f },{ 0.2f, 0.2f, 1.0f, 1.0f } }
+	};
+
+	GLenum ErrorCheckValue = glGetError();
+	const size_t BufferSize = sizeof(Vertices);
+	const size_t VertexSize = sizeof(Vertices[0]);
+	const size_t RgbOffset = sizeof(Vertices[0].XYZW);
+
+	glGenVertexArrays(1, &VaoId);
+	glBindVertexArray(VaoId);
+
+	glGenBuffers(1, &VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, VboId);
+	glBufferData(GL_ARRAY_BUFFER, BufferSize, Vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, VertexSize, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, VertexSize, (GLvoid*)RgbOffset);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not create a VBO: %s \n",
+			gluErrorString(ErrorCheckValue)
+			);
+
+		exit(-1);
+	}
+}
+
+void DestroyVBO(void)
+{
+	GLenum ErrorCheckValue = glGetError();
+
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VboId);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VaoId);
+
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not destroy the VBO: %s \n",
+			gluErrorString(ErrorCheckValue)
+			);
+
+		exit(-1);
+	}
+}
+
+void CreateShaders(void)
+{
+	GLenum ErrorCheckValue = glGetError();
+
+	VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+	glCompileShader(VertexShaderId);
+
+	FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+	glCompileShader(FragmentShaderId);
+
+	ProgramId = glCreateProgram();
+	glAttachShader(ProgramId, VertexShaderId);
+	glAttachShader(ProgramId, FragmentShaderId);
+	glLinkProgram(ProgramId);
+	glUseProgram(ProgramId);
+
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not create the shaders: %s \n",
+			gluErrorString(ErrorCheckValue)
+			);
+
+		exit(-1);
+	}
+}
+
+void DestroyShaders(void)
+{
+	GLenum ErrorCheckValue = glGetError();
+
+	glUseProgram(0);
+
+	glDetachShader(ProgramId, VertexShaderId);
+	glDetachShader(ProgramId, FragmentShaderId);
+
+	glDeleteShader(FragmentShaderId);
+	glDeleteShader(VertexShaderId);
+
+	glDeleteProgram(ProgramId);
+
+	ErrorCheckValue = glGetError();
+	if (ErrorCheckValue != GL_NO_ERROR)
+	{
+		fprintf(
+			stderr,
+			"ERROR: Could not destroy the shaders: %s \n",
+			gluErrorString(ErrorCheckValue)
+			);
+
+		exit(-1);
+	}
 }
