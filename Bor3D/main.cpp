3c66c37efe1bb4f9cb7fb46a1a5759a61190a5c6
Ciacco Davide <ciaccodavide@gmail.com>
2017-10-25 21:27:15 +0200

    first triangle and simplest shader

--- Bor3D/main.cpp
+++ Bor3D/main.cpp
@@ -4,6 +4,18 @@
 #include <SDL_opengl.h>
 
 const GLint WIDTH = 980, HEIGHT = 640;
+const GLchar *vertexShaderSource = "#version 440 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}";
+const GLchar *fragmentShaderSource = "#version 440 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}";
 
 int main(int argc, char *argv[])
 {
@@ -19,7 +31,7 @@ int main(int argc, char *argv[])
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
 	// create a new window
-	SDL_Window *window = SDL_CreateWindow("Bor3D", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+	SDL_Window *window = SDL_CreateWindow("Bor3D", 30, 60, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
 
 	// create a new context
 	SDL_GLContext context = SDL_GL_CreateContext(window);
@@ -38,6 +50,77 @@ int main(int argc, char *argv[])
 
 	SDL_Event windowEvent;
 
+	// vertex shader creation
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// compile vertex shader
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+	// check compilation errors
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// fragment shader creator
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// compile fragment shader
+	glCompileShader(fragmentShader);
+	// check compilation errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// create a shader program and attach the shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// check linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// we can now delete our shaders (they are inside our program)
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// vertices. [0,0] is the middle
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f, // bottom left
+		0.5f, -0.5f, 0.0f, // bottom left
+		0.0f, 0.5f, 0.0f, // top middle
+	};
+
+	GLuint VBO, VAO; // vertex buffer object and vertex array object
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//unbind
+	glBindVertexArray(0);
+
+	// game loop
 	while (true)
 	{
 		if (SDL_PollEvent(&windowEvent))
@@ -53,12 +136,22 @@ int main(int argc, char *argv[])
 
 
 		// DRAW OPENGL HERE
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// unbind
+		glBindVertexArray(0);
 
 
 		// if double buffering is present this will swap, otherwise not
 		SDL_GL_SwapWindow(window);
 	}
 
+	//delete
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// delete&destroy before exiting
 	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
